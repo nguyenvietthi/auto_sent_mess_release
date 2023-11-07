f576d6b661c48fe0b775f5085c30c77b0003da2e
Thi <nguyenvietthi.nd@gmail.com>
2023-11-07 16:25:29 +0700

    first commit

--- gui.py
+++ gui.py
@@ -2,85 +2,50 @@ import tkinter as tk
 from tkinter import scrolledtext
 import auto_sent_mess_fb
 import threading
-import time
-
-should_exit = False
-paused = False
-current_thread = None
 
 def on_button_click():
-    global should_exit, key_word_reg, country_reg, paused, current_thread
-
-    user_name = text_boxes[0].get()
-    pass_word = text_boxes[1].get()
+    # Lấy dữ liệu từ các ô text box
+    user_name  = text_boxes[0].get()
+    pass_word  = text_boxes[1].get()
     secret_2fa = text_boxes[2].get()
-    country = text_boxes[3].get()
-    key_word = text_boxes[4].get()
-    mess_text = text_editor.get("1.0", tk.END)
+    country    = text_boxes[3].get()
+    key_word  = text_boxes[4].get()
+    mess_text  = text_editor.get("1.0", tk.END)
 
-    # Dừng thread hiện tại nếu có
-    if current_thread and current_thread.is_alive():
-        should_exit = True
-        current_thread.join()  # Đợi thread hiện tại kết thúc
-
-    should_exit = False  # Đặt lại biến cờ cho thread mới
-
-    if (user_name != "" and pass_word != "" and secret_2fa != "" and country != "" and key_word != "" and mess_text != ""):
+    if(user_name != "" and pass_word != "" and secret_2fa != "" and country != "" and key_word != "" and mess_text != ""):
+        # Hiển thị thông báo
         message_label.config(text="Dữ liệu đã được lấy thành công!", fg="green")
-
-        if button.cget("text") == "Run":
-            # Tạo và chạy thread mới
-            current_thread = threading.Thread(target=worker_thread, args=(user_name, pass_word, secret_2fa, country, key_word, mess_text, ))
-            current_thread.start()
-            button.configure(text="Pause")
-        elif button.cget("text") == "Pause":
-            button.configure(text="Resume")
-            paused = True
-        elif button.cget("text") == "Resume":
-            resume_thread(user_name, pass_word, secret_2fa, country, key_word, mess_text)
-
+        threading.Thread(target=worker_thread, args=(user_name, pass_word, secret_2fa, country, key_word, mess_text,)).start()
     else:
         message_label.config(text="Vui lòng nhập đủ thông tin", fg="red")
 
+# Hàm chạy khi bạn đóng cửa sổ
 def on_closing():
     global should_exit
     should_exit = True
     root.destroy()
 
 def update_message(message, color):
+    # Cập nhật thông báo trên GUI
     message_label.config(text=message, fg=color)
 
 def worker_thread(user_name, pass_word, secret_2fa, country, key_word, mess_text):
-    global should_exit, paused
+    # Thực hiện công việc nặng nề ở đây
+    print("ASdasdasdasdas", user_name, pass_word, secret_2fa, country, key_word, mess_text)
+    auto_sent_mess_fb.run_auto(user_name, pass_word, secret_2fa, country, key_word, mess_text)
 
-    while not should_exit:
-        if not paused:
-            print("INFO:", user_name, pass_word, secret_2fa, country, key_word, mess_text)
-            auto_sent_mess_fb.run_auto(user_name, pass_word, secret_2fa, country, key_word, mess_text)
-            root.after(0, lambda: update_message("Công việc nặng nề đã hoàn thành!", "blue"))
-            time.sleep(1)
-        else:
-            time.sleep(1)
+    # Sau khi hoàn thành công việc, cập nhật thông báo trên GUI
+    root.after(0, lambda: update_message("Công việc nặng nề đã hoàn thành!", "blue"))
 
-    print("Thread stopped")
-
-def resume_thread(user_name, pass_word, secret_2fa, country, key_word, mess_text):
-    global paused, current_thread
-
-    paused = False
-    if current_thread and current_thread.is_alive():
-        should_exit = True
-        current_thread.join()  # Đợi thread hiện tại kết thúc
-
-    should_exit = False
-    current_thread = threading.Thread(target=worker_thread, args=(user_name, pass_word, secret_2fa, country, key_word, mess_text,))
-    current_thread.start()
-
+# Tạo cửa sổ chính
 root = tk.Tk()
 root.title("Auto send messages for Facebook")
 
-button = tk.Button(root, text="Run", command=on_button_click)
+# Tạo nút bấm
+button = tk.Button(root, text="RUN AUTO", command=on_button_click)
 button.grid(row=0, column=1, pady=10)
+
+# Tạo 5 ô text box và nhãn tương ứng
 text_boxes = []
 
 label = tk.Label(root, text=f"User Name:")
